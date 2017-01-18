73fe00ff9087f434876552eae77d3aa8425b43ec
HQ\terry.robinson <terry.robinson@hronline.co.uk>
2017-01-18 07:50:21 +0000

    Implemented Interface and class for Frame and created failing test for score

---
+++ BowlingOO/BowlingOO/Frame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingOO
+{
+    public class Frame
+    {
+        public Frame(string frameRolls)
+        {
+            
+        }
+
+        public int Score()
+        {
+            return 0;
+        }
+    }
+}
---
+++ BowlingOO/BowlingOOTests/FrameTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BowlingOO;
+using NUnit.Framework;
+
+namespace BowlingOOTests
+{
+    [TestFixture]
+    public class FrameTests
+    {
+        [Test]
+        public void TestFrameScoreWhenFrameIsOnlyNumbers()
+        {
+            var frame = new Frame("12");
+            Assert.That(frame.Score(), Is.EqualTo(3));
+        }
+    }
+}
