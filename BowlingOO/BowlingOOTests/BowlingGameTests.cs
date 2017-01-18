420b52db937a05269fcbaa464ee4468732b84e39
Terry Robinson <terry@saratez.co.uk>
2017-01-18 18:12:38 +0000

    Added test for missing requirement - if the 2 bonus balls end in a spare. Test passed.

--- BowlingOO/BowlingOOTests/BowlingGameTests.cs
+++ BowlingOO/BowlingOOTests/BowlingGameTests.cs
@@ -26,6 +26,7 @@ namespace BowlingOOTests
         [TestCase("5/|5/|5/|5/|5/|5/|5/|5/|5/|5/||5", 150)]
         [TestCase("X|7/|9-|X|-8|8/|-6|X|X|X||81", 167)]
         [TestCase("X|X|X|X|X|X|X|X|X|X||XX", 300)]
+        [TestCase("X|7/|9-|X|-8|8/|-6|X|X|X||8/", 168)]
         public void TestGameScore(string gameScore, int expectedScore)
         {
             var game = new BowlingGame(gameScore);
