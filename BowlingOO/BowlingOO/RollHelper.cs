7648351e059837001641edf9db1c2cfb4b4d131d
Terry Robinson <terry@saratez.co.uk>
2017-01-18 18:07:01 +0000

    Gutter ball and strike tests for GetRollValue passing

--- BowlingOO/BowlingOO/RollHelper.cs
+++ BowlingOO/BowlingOO/RollHelper.cs
@@ -11,6 +11,16 @@ namespace BowlingOO
         public const int ASCII_OFFSET = 48;
         public static int GetRollValue(this char roll)
         {
+            if (roll == '-')
+            {
+                return 0;
+            }
+
+            if (roll == 'X')
+            {
+                return 10;
+            }
+
             return roll - ASCII_OFFSET;
         }
     }
--- BowlingOO/BowlingOOTests/RollHelperTests.cs
+++ BowlingOO/BowlingOOTests/RollHelperTests.cs
@@ -13,6 +13,7 @@ namespace BowlingOOTests
     {
         [TestCase('1', 1)]
         [TestCase('-', 0)]
+        [TestCase('X', 10)]
         public void TestRollValue(char roll, int expectedValue)
         {
             Assert.That(roll.GetRollValue(), Is.EqualTo(expectedValue));
