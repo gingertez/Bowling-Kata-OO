e25ede5c736d80b0bfbb044b5d563ab912135411
HQ\terry.robinson <terry.robinson@hronline.co.uk>
2017-01-18 09:12:47 +0000

    Created frame factory - tests created but method not implemented

---
+++ BowlingOO/BowlingOO/FrameFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingOO
+{
+    public static class FrameFactory
+    {
+        public static IFrame GetFrame(string frameScore)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
